a9c107d633f65eef182e858cf6a118386d06276a
Mr.Sabith.A5 <135003378+NotoriousBusinessMan@users.noreply.github.com>
2023-12-28 07:31:39 +0000

    hi33

--- src/purelink/web/views.py
+++ src/purelink/web/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from web.models import Donor
 from web.forms import DonorForm
 
 
@@ -11,16 +14,32 @@ def donate_blood(request):
     if request.method == 'POST':
         form = DonorForm(request.POST)
         if form.is_valid():
-            form.save()
-
-            response_data = {
-                "status": "success",
-                "value": "success",
-                'title': "Successfully Registered",
-                "message": "You have successfully registered to our newsletter"
-            }
-
-            return render(request, 'empty.html', response_data)
+            phone_number = form.cleaned_data['phone_no']
+            
+            # Check if a user with the given phone number already exists
+            existing_user = Donor.objects.filter(phone_no=phone_number).first()
+            
+            if existing_user:
+                # User already registered, provide a message
+                response_data = {
+                    "status": "error",
+                    "value": "error",
+                    'title': "Already Registered",
+                    "message": "A user with this phone number is already registered."
+                }
+            else:
+                # User not registered, save the form
+                form.save()
+                
+                response_data = {
+                    "status": "success",
+                    "value": "success",
+                    'title': "Successfully Registered",
+                    "message": "You have successfully registered to our newsletter"
+                }
+            
+            return HttpResponse(json.dumps(response_data), content_type="application/json")
+    
     else:
         form = DonorForm()
 
